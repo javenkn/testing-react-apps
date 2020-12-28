b11c92205f4f6606e25ddcecb17c3d88ea5bb8b2
javenkn <javenkn7@gmail.com>
2020-12-28 12:55:31 -1000

    Replaces click with event api MouseEvent

--- src/__tests__/exercise/01.js
+++ src/__tests__/exercise/01.js
@@ -14,10 +14,16 @@ test('counter increments and decrements when the buttons are clicked', () => {
   const [decrement, increment] = div.querySelectorAll('button')
   const message = div.firstChild.querySelector('div')
 
+  const clickEvent = new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true,
+    button: 0,
+  })
+
   expect(message.textContent).toBe('Current count: 0')
-  increment.click()
+  increment.dispatchEvent(clickEvent)
   expect(message.textContent).toBe('Current count: 1')
-  decrement.click()
+  decrement.dispatchEvent(clickEvent)
   expect(message.textContent).toBe('Current count: 0')
 
   div.remove()
