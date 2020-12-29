557d81256ee0804d463217da0a20ad150a494127
javenkn <javenkn7@gmail.com>
2020-12-28 15:40:08 -1000

    Implements test-data-bot into the test

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -4,22 +4,21 @@
 import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import faker from 'faker'
 import Login from '../../components/login'
+import {build, fake} from '@jackfranklin/test-data-bot'
 
-function buildLoginForm(overrides) {
-  return {
-    username: faker.internet.userName(),
-    password: faker.internet.password(),
-    ...overrides,
-  }
-}
+const buildLoginForm = build({
+  fields: {
+    username: fake(f => f.internet.userName()),
+    password: fake(f => f.internet.password()),
+  },
+})
 
 test('submitting the form calls onSubmit with username and password', () => {
   const handleSubmit = jest.fn()
   render(<Login onSubmit={handleSubmit} />)
 
-  const {username, password} = buildLoginForm({username: 'adslkfjasdlkj'})
+  const {username, password} = buildLoginForm()
   const usernameInput = screen.getByLabelText(/username/i)
   const passwordInput = screen.getByLabelText(/password/i)
   userEvent.type(usernameInput, username)
