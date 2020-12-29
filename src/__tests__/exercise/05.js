9c78eaaa4cbecbe3f6751f26e1a5f84c2a78e08b
javenkn <javenkn7@gmail.com>
2020-12-28 16:22:53 -1000

    Adds another test for testing unknown server error

--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -13,6 +13,7 @@ import {build, fake} from '@jackfranklin/test-data-bot'
 import Login from '../../components/login-submission'
 import {setupServer} from 'msw/node'
 import {handlers} from 'test/server-handlers'
+import {rest} from 'msw'
 
 const buildLoginForm = build({
   fields: {
@@ -25,6 +26,7 @@ const server = setupServer(...handlers)
 
 beforeAll(() => server.listen())
 afterAll(() => server.close())
+afterEach(() => server.resetHandlers())
 
 test(`logging in displays the user's username`, async () => {
   render(<Login />)
@@ -53,4 +55,23 @@ test(`logging in without password displays error`, async () => {
   expect(screen.getByRole(/alert/i).textContent).toMatchInlineSnapshot(
     `"password required"`,
   )
+})
+
+test(`unknown server error displays the error message`, async () => {
+  const testErrorMessage = 'Something is wrong'
+  server.use(
+    rest.post(
+      'https://auth-provider.example.com/api/login',
+      async (req, res, ctx) => {
+        return res(ctx.status(500), ctx.json({message: testErrorMessage}))
+      },
+    ),
+  )
+  render(<Login />)
+  userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  const spinner = screen.getByLabelText(/loading/i)
+  await waitForElementToBeRemoved(spinner)
+
+  expect(screen.getByRole(/alert/i)).toHaveTextContent(testErrorMessage)
 })
